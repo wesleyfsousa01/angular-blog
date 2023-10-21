7f814686574db74d4b753be66a2e104b8986d047
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-10-21 12:10:14 -0300

    first commit

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  id:string = "0"
+
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardTitle:string = ""
+
+  constructor () {
+
+  }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO",
+    "description":"Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover" : "https://www.einerd.com.br/wp-content/uploads/2019/12/homem-de-ferro-3-e1576841000341-890x464.jpg"
+  },
+
+  {
+    "id":"2",
+    "title":"HOMEM ARANHA SEM VOLTA PRA CASA",
+    "description":"Novo filme do homem aranha terá participação de atores de filmes anteriores",
+    "photoCover" : "https://labdicasjornalismo.com/images/noticias/10139/25012022231802_legiao_kwU.jpeg"
+  }
+]
